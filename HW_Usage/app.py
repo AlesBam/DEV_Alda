4fb0b325b9d0ca2a2b67f92c0979cc35bd1b27a1
AlesBam <alesbambas@gmail.com>
2024-01-13 10:38:58 +0100

    Aktualizace na funkce

--- HW_Usage/app.py
+++ HW_Usage/app.py
@@ -2,37 +2,28 @@ import psutil
 from colorama import Fore, Style
 import time
 
+def print_device_usage(device, device_type):
+    if 0 <= device < 50:
+        print(f"{Fore.GREEN}{device_type} Usage: {device}%{Style.RESET_ALL}")
+    elif 50 <= device < 75:
+        print(f"{Fore.YELLOW}{device_type} Usage: {device}%{Style.RESET_ALL}")
+    elif device >= 75:
+        print(f"{Fore.RED}{device_type} Usage: {device}%{Style.RESET_ALL}")
+
+def print_network_usage(network):
+    if network > 0:
+        print(f"{Fore.GREEN}Network Usage: {network} bytes{Style.RESET_ALL}\n")
+
 def print_usage():
     cpu_usage = psutil.cpu_percent(interval=1)
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
     network_usage = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
 
-    print()
-
-    if 0 <= cpu_usage < 50:
-        print(f"{Fore.GREEN}CPU Usage: {cpu_usage}%{Style.RESET_ALL}")
-    elif 50 <= cpu_usage < 75:
-        print(f"{Fore.YELLOW}CPU Usage: {cpu_usage}%{Style.RESET_ALL}")
-    elif cpu_usage >= 75:
-        print(f"{Fore.RED}CPU Usage: {cpu_usage}%{Style.RESET_ALL}")
-
-    if 0 <= ram_usage < 50:
-        print(f"{Fore.GREEN}RAM Usage: {ram_usage}%{Style.RESET_ALL}")
-    elif 50 <= ram_usage < 75:
-        print(f"{Fore.YELLOW}RAM Usage: {ram_usage}%{Style.RESET_ALL}")
-    elif ram_usage >= 75:
-        print(f"{Fore.RED}RAM Usage: {ram_usage}%{Style.RESET_ALL}")
-
-    if 0 <= disk_usage < 50:
-        print(f"{Fore.GREEN}Disk Usage: {disk_usage}%{Style.RESET_ALL}")
-    elif 50 <= disk_usage < 75:
-        print(f"{Fore.YELLOW}Disk Usage: {disk_usage}%{Style.RESET_ALL}")
-    elif disk_usage >= 75:
-        print(f"{Fore.RED}Disk Usage: {disk_usage}%{Style.RESET_ALL}")
-
-    if network_usage > 0:
-        print(f"{Fore.GREEN}Network Usage: {network_usage} bytes{Style.RESET_ALL}")
+    print_device_usage(cpu_usage, "CPU")
+    print_device_usage(ram_usage, "RAM")
+    print_device_usage(disk_usage,"Disk")
+    print_network_usage(network_usage)
 
 if __name__ == "__main__":
     while True:
